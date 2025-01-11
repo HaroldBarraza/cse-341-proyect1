fe3d7493b688ebdc722fc836668fef4ed1af1912
HaroldBarraza <132396790+HaroldBarraza@users.noreply.github.com>
2025-01-10 19:19:21 -0500

    Step5 Add get and getall

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const express = require('express');
+
+const mongodb = require('./data/database');
+
 const app = express();
 
 const port = process.env.PORT || 3000;
 
 app.use('/', require('./routes'));
 
-app.listen(port, () => (console.log(`Server started on port ${port}`)));
+
+mongodb.intDb((err) => {
+    if (err) {
+        console.log(err)
+    }else{
+        app.listen(port, () => (console.log(`Data Base is listening and Server started on port ${port}`)));
+    }
+}) 
